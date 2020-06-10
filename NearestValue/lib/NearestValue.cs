83ac5c42ec93aadbe8158ae9cb1fc5a62ad100e0
Dean Agan <agandfr@gmail.com>
2020-06-10 17:33:48 +1000

    Add nearest value

---
+++ NearestValue/lib/NearestValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lib
+{
+    public class NearestValue
+    {
+        public int Result(int[] values, int search)
+        {
+            return 0;
+        }
+    }
+}
---
+++ NearestValue/test/NearestValueTest.cs
@@ -0,0 +1,29 @@
+
+using Xunit;
+using FluentAssertions;
+
+namespace lib.test
+{
+    public class NearestValueShould
+    {
+        [Theory]
+        [InlineData(new object[] {new int[] {4, 7, 10, 11, 12, 17}, 9, 10})]
+        [InlineData(new object[] {new int[] {4, 7, 10, 11, 12, 17}, 8, 7})]
+        [InlineData(new object[] {new int[] {4, 8, 10, 11, 12, 17}, 9, 8})]
+        [InlineData(new object[] {new int[] {4, 9, 10, 11, 12, 17}, 9, 9})]
+        [InlineData(new object[] {new int[] {4, 7, 10, 11, 12, 17}, 0, 4})]
+        [InlineData(new object[] {new int[] {4, 7, 10, 11, 12, 17}, 100, 17})]
+        [InlineData(new object[] {new int[] {5, 10, 8, 12, 89, 100}, 7, 8})]
+        [InlineData(new object[] {new int[] {-1, 2, 3}, 0, -1})]
+        [InlineData(new object[] {new int[] {0, -2}, -1, -2})]
+        public void BeEqual_WhenGivenNumbers(int[] values, int search, int expected)
+        {
+            // Arrange
+            var nv = new NearestValue();
+            // Act
+            var result = nv.Result(values, search);
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
