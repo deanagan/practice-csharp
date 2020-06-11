4a2e72a15d459e630326af3d377da7beefe4ee6d
Dean Agan <agandfr@gmail.com>
2020-06-11 17:29:54 +1000

    Add solution to pass all tests

--- NearestValue/lib/NearestValue.cs
+++ NearestValue/lib/NearestValue.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace lib
 {
+    class SearchDistanceComparer : IComparer<int>
+    {
+        private readonly int _search;
+        public SearchDistanceComparer(int search)
+        {
+            _search = search;
+        }
+        public int Compare(int a, int b)
+        {
+            var result = Math.Abs(_search - a) - Math.Abs(_search - b);
+            if (result == 0)
+            {
+                return Math.Min(a,b);
+            }
+            return result;
+        }
+    }
     public class NearestValue
     {
         public int Result(int[] values, int search)
         {
-            var res = values.Select(value => Tuple.Create(value, Math.Abs(value-search))).OrderBy(p => Math.Abs(p.Item1 - search) + p.Item1);
-            return res.First().Item1;
+            var res = values.OrderBy(v => v, new SearchDistanceComparer(search));
+            return res.First();
         }
     }
 }
