f0cb1753e56533844795b026ad83da170518af76
Dean Agan <agandfr@gmail.com>
2020-09-25 16:56:53 +1000

    Fixed tests

--- Test/ValidParenthesesTest.cs
+++ Test/ValidParenthesesTest.cs
@@ -7,13 +7,13 @@ namespace lib.test
     public class ValidParenthesesShould
     {
         [Theory]
-        // [InlineData(new object[] {"()[]{}", true })]
-        // [InlineData(new object[] {"(]", false })]
-        // [InlineData(new object[] {"([)]", false })]
+        [InlineData(new object[] {"()[]{}", true })]
+        [InlineData(new object[] {"(]", false })]
+        [InlineData(new object[] {"([)]", false })]
         [InlineData(new object[] {"{[]}", true })]
-        // [InlineData(new object[] {"]", false })]
-        // [InlineData(new object[] {"(])", false })]
-        // [InlineData(new object[] {"){", false })]
+        [InlineData(new object[] {"){", false })]
+        [InlineData(new object[] {"]", false })]
+        [InlineData(new object[] {"(])", false })]
         public void BeEqualToExpected_WhenInputs(string input, bool expected)
         {
             // Arrange
