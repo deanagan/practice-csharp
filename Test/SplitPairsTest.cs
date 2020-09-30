ed94ad0c38a7255d619ced274b1d02b464a8cec1
Dean Agan <agandfr@gmail.com>
2020-09-30 19:32:36 +1000

    Created split pairs test

---
+++ Test/SplitPairsTest.cs
@@ -0,0 +1,26 @@
+
+using Xunit;
+using FluentAssertions;
+using System.Collections.Generic;
+
+namespace lib.test
+{
+    public class SplitPairsTest
+    {
+        [Theory]
+        [InlineData(new object[] {"abcd", new string[]{"ab", "cd"} })]
+        [InlineData(new object[] {"abc", new string[]{"ab", "c_"} })]
+        [InlineData(new object[] {"abcdf", new string[]{"ab","cd", "f_"} })]
+        [InlineData(new object[] {"a", new string[]{"a_"} })]
+        [InlineData(new object[] {"", new string[]{} })]
+        public void ShouldGet_Correct_SplitUpList(string input, string[] expected)
+        {
+            // Arrange
+            var sp = new SplitPairs();
+            // Act
+            var result = sp.SplitString(input);
+            // Assert
+            result.Should().Equal(new List<string>(expected));
+        }
+    }
+}
