b91a7aaeeb4828186b1defefce27097235a6b858
Dean Agan <agandfr@gmail.com>
2020-11-04 13:37:40 +1100

    Add test

---
+++ Test/NumberPairsTest.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace lib.test
+{
+    public class TestNumberPairs
+    {
+        [Theory]
+        [InlineData(new object[] {new int[]{[5, 25, 45, 15]}, 30, 2)]
+        public void ShouldReturnCorrectCount_WhenGivenInputs(int[] inputs, int mod, int expected)
+        {
+            // Arrange
+            var sut = new NumberPairs(inputs);
+            // Act
+            var result = sut.CountPairs(mod);
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
