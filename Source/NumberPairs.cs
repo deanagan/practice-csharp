916fef36bf56898ef866e0cb768d4a3a2bce5e88
Dean Agan <agandfr@gmail.com>
2020-11-04 13:42:31 +1100

    Implement number pairs so it compiles

---
+++ Source/NumberPairs.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace lib
+{
+    public class NumberPairs
+    {
+        List<int> _numbers;
+        public NumberPairs(int[] inputs)
+        {
+            _numbers = new List<int>(inputs);
+        }
+        public int CountPairs(int mod)
+        {
+            return 0;
+        }
+    }
+}
