75f71faa5b74e9af5539c9ffbbd9d3bf8d73c7c5
Dean Agan <agandfr@gmail.com>
2020-11-04 13:42:12 +1100

    Fixed error in test

--- Test/NumberPairsTest.cs
+++ Test/NumberPairsTest.cs
@@ -7,7 +7,7 @@ namespace lib.test
     public class TestNumberPairs
     {
         [Theory]
-        [InlineData(new object[] {new int[]{[5, 25, 45, 15]}, 30, 2)]
+        [InlineData(new object[] {new int[]{5, 25, 45, 15}, 30, 2})]
         public void ShouldReturnCorrectCount_WhenGivenInputs(int[] inputs, int mod, int expected)
         {
             // Arrange
